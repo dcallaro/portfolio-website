9f733e759b84cf02fd6174a01dcebe891820b4a5
dcallaro <devoncallaro@gmail.com>
2021-02-11 11:48:55 -0500

    Adding hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+console.log ("Welcome to my portflio site!")
 
-
-  console.log ("Welcome to my portflio site!")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+}
